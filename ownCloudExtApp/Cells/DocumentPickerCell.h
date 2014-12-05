52cd42de7287ed2160be00aa804450af9506c950
Gonzalo Gonzalez <ggonzalez@apperian.com>
2014-12-05 13:43:00 +0100

    Added new cell specific for DocumentPickerCell

---
+++ ownCloudExtApp/Cells/DocumentPickerCell.h
@@ -0,0 +1,13 @@
+//
+//  DocumentPickerCell.h
+//  Owncloud iOs Client
+//
+//  Created by Gonzalo Gonzalez on 5/12/14.
+//
+//
+
+#import "CustomCellFileAndDirectory.h"
+
+@interface DocumentPickerCell : CustomCellFileAndDirectory
+
+@end
