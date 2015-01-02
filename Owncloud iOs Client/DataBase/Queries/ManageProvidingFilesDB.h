2f10d26d06d36620ffd56b8bc6de2c9ff7b80ead
Gonzalo Gonzalez <ggonzalez@apperian.com>
2015-01-02 12:30:46 +0100

    Added class to manage the ProvidingFiles

---
+++ Owncloud iOs Client/DataBase/Queries/ManageProvidingFilesDB.h
@@ -0,0 +1,32 @@
+//
+//  ManageProvidingFilesDB.h
+//  Owncloud iOs Client
+//
+// This class represents the files that are using in other apps by Document Provider and
+// it's can be edited by these other apps.
+//
+//  Created by Gonzalo Gonzalez on 2/1/15.
+//
+
+/*
+ Copyright (C) 2015, ownCloud, Inc.
+ This code is covered by the GNU Public License Version 3.
+ For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ You should have received a copy of this license
+ along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
+
+#import <Foundation/Foundation.h>
+
+@class ProvidingFileDto;
+
+@interface ManageProvidingFilesDB : NSObject
+
++ (ProvidingFileDto *) insertProvidingFileDtoNamed:(NSString *)fileName withPath:(NSString*)filePath ofUser:(NSInteger)userId;
+
++ (BOOL) removeProvidingFileDtoById:(NSInteger)idProvidingFile;
+
++ (NSArray*) getAllProvidingFilesDtoOfUser:(NSInteger)userId;
+
+
+@end
---
+++ Owncloud iOs Client/DataBase/Queries/ManageProvidingFilesDB.m
@@ -0,0 +1,166 @@
+//
+//  ManageProvidingFilesDB.m
+//  Owncloud iOs Client
+//
+// This class represents the files that are using in other apps by Document Provider and
+// it's can be edited by these other apps.
+//
+//  Created by Gonzalo Gonzalez on 2/1/15.
+//
+
+
+/*
+ Copyright (C) 2015, ownCloud, Inc.
+ This code is covered by the GNU Public License Version 3.
+ For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ You should have received a copy of this license
+ along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
+
+#import "ManageProvidingFilesDB.h"
+#import "FMDatabaseQueue.h"
+#import "FMDatabase.h"
+#import "ProvidingFileDto.h"
+#import "DocumentPickerViewController.h"
+
+#ifdef CONTAINER_APP
+#import "AppDelegate.h"
+#else
+#import "DocumentPickerViewController.h"
+#endif
+
+
+@implementation ManageProvidingFilesDB
+
+/*
+ * Method that return last user inserted on the Database
+ */
++ (ProvidingFileDto *) getTheLastProvidingFileInserted {
+    
+    __block ProvidingFileDto *output = nil;
+    
+    output=[ProvidingFileDto new];
+    
+    FMDatabaseQueue *queue;
+    
+#ifdef CONTAINER_APP
+    queue = [AppDelegate sharedDatabase];
+#else
+    queue = [DocumentPickerViewController sharedDatabase];
+#endif
+    
+    [queue inDatabase:^(FMDatabase *db) {
+        FMResultSet *rs = [db executeQuery:@"SELECT id, file_path, file_name, user_id FROM providing_files ORDER BY id DESC LIMIT 1"];
+        
+        while ([rs next]) {
+            
+            output.idProvidingFile = [rs intForColumn:@"id"];
+            output.filePath = [rs stringForColumn:@"file_path"];
+            output.fileName = [rs stringForColumn:@"file_name"];
+            output.userId = [rs intForColumn:@"user_id"];
+            
+        }
+        
+        [rs close];
+        
+    }];
+    
+    return output;
+}
+
+
++ (ProvidingFileDto *) insertProvidingFileDtoNamed:(NSString *)fileName withPath:(NSString*)filePath ofUser:(NSInteger)userId{
+    
+    FMDatabaseQueue *queue;
+    
+#ifdef CONTAINER_APP
+    queue = [AppDelegate sharedDatabase];
+#else
+    queue = [DocumentPickerViewController sharedDatabase];
+#endif
+    
+    __block BOOL correctQuery = NO;
+    
+    [queue inTransaction:^(FMDatabase *db, BOOL *rollback) {
+        
+        correctQuery = [db executeUpdate:@"INSERT INTO providing_files(file_path, file_name, user_id) Values(?, ?, ?)", filePath, fileName, [NSNumber numberWithInteger:userId]];
+        
+        if (!correctQuery) {
+            DLog(@"Error added ProvidingFile");
+        }
+        
+    }];
+    
+    ProvidingFileDto *providingFileDto = nil;
+    
+    if (correctQuery) {
+        providingFileDto = [self getTheLastProvidingFileInserted];
+    }
+    
+    return providingFileDto;
+    
+}
+
++ (BOOL) removeProvidingFileDtoById:(NSInteger)idProvidingFile{
+    
+    __block BOOL correctQuery = NO;
+    
+    FMDatabaseQueue *queue;
+    
+#ifdef CONTAINER_APP
+    queue = [AppDelegate sharedDatabase];
+#else
+    queue = [DocumentPickerViewController sharedDatabase];
+#endif
+    
+    [queue inTransaction:^(FMDatabase *db, BOOL *rollback) {
+        BOOL correctQuery=NO;
+        
+        correctQuery = [db executeUpdate:@"DELETE FROM providing_files WHERE id = ?", [NSNumber numberWithInteger:idProvidingFile]];
+        
+        if (!correctQuery) {
+            DLog(@"Error delete providing file");
+            
+        }
+        
+    }];
+    
+    return correctQuery;
+
+}
+
++ (NSArray*) getAllProvidingFilesDtoOfUser:(NSInteger)userId{
+    
+    __block NSMutableArray *tempArray = [NSMutableArray new];
+    
+    FMDatabaseQueue *queue;
+    
+#ifdef CONTAINER_APP
+    queue = [AppDelegate sharedDatabase];
+#else
+    queue = [DocumentPickerViewController sharedDatabase];
+#endif
+    
+    [queue inDatabase:^(FMDatabase *db) {
+        FMResultSet *rs = [db executeQuery:@"SELECT id, file_path, file_name, user_id FROM providing_files ORDER BY id DESC"];
+        
+        while ([rs next]) {
+            
+            ProvidingFileDto *providerFileTemp = [ProvidingFileDto new];
+            
+            providerFileTemp.idProvidingFile = [rs intForColumn:@"id"];
+            providerFileTemp.filePath = [rs stringForColumn:@"file_path"];
+            providerFileTemp.fileName = [rs stringForColumn:@"file_name"];
+            providerFileTemp.userId = [rs intForColumn:@"user_id"];
+            
+            [tempArray addObject:providerFileTemp];
+        }
+        
+        [rs close];
+        
+    }];
+    
+    return [NSArray arrayWithArray:tempArray];;
+}
+
+@end
