31666798ccf43ff03c2575fa5d86550559dd14ac
Gonzalo Gonzalez <ggonzalez@apperian.com>
2015-03-04 17:32:41 +0100

    Added the share-in extension in swift

---
+++ OC Share Sheet/ShareViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ShareViewController.swift
+//  OC Share Sheet
+//
+//  Created by Gonzalo Gonzalez on 4/3/15.
+//
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [AnyObject]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return NSArray()
+    }
+
+}
