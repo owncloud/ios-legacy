fec6f6e819b7fab670a047894c0268f51019a014
Gonzalo Gonzalez <ggonzalez@apperian.com>
2015-04-08 17:14:17 +0200

    Added copyright headers and remove an example class

--- Owncloud iOs Client/Owncloud iOs Client-Bridging-Header.h
+++ Owncloud iOs Client/Owncloud iOs Client-Bridging-Header.h
@@ -2,6 +2,14 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+/*
+ Copyright (C) 2015, ownCloud, Inc.
+ This code is covered by the GNU Public License Version 3.
+ For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ You should have received a copy of this license
+ along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
+
 #import "UtilsUrls.h"
 #import "FileNameUtils.h"
 #import "UIColor+Constants.h"
