433ebc3f82c7225ef73190510a297fb31ff05acd
Javier Gonzalez <javiergonzper@gmail.com>
2015-07-09 15:33:48 +0200

    Added the widget with the buttons to access to the tabs but without funcionality by now

---
+++ OCTodayWidget/TodayViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TodayViewController.swift
+//  OCTodayWidget
+//
+//  Created by Javier Gonzalez on 9/7/15.
+//
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+}
