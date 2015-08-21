4f5ca1cdf22e0c9e532636e3fc415943d89de3c3
Javier Gonzalez <javiergonzper@gmail.com>
2015-08-21 15:03:53 +0200

    Added the way to create the folder with appium but working on check if it exist

--- automation_Test/createFolderTest.py
+++ automation_Test/createFolderTest.py
@@ -12,6 +12,15 @@ import filesView
 import settingsView
 import actions
 
+class createFolderTest(unittest.TestCase):
+
+    def setUp(self):
+        # set up appium
+        self.driver = actions.getWebDriver()
+        #self.driver.implicitly_wait(60)
+
+    def tearDown(self):
+        self.driver.quit()
 
     def test_create_folder_ok(self):
         server_url = const.K_URL_1
@@ -20,5 +29,26 @@ import actions
         ssl = const.K_SELF_SIGNED_1
 
         actions.doFirstLoginWith(self,server_url,user,password,ssl)
+        sleep(1)
+        #self.driver.find_element_by_name("Add").click()
+        #sleep(1)
+        #self.driver.find_element_by_name("New folder").click()
+        #sleep(1)
+        #self.driver.find_element_by_xpath("//UIAApplication[1]/UIAWindow[3]/UIAAlert[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIATextField[1]").set_value("aHola")
+        #self.driver.find_element_by_xpath("//UIAApplication[1]/UIAWindow[3]/UIAAlert[1]/UIACollectionView[1]/UIACollectionCell[2]").click()
+        #sleep(3)
+        
+        #this works
+        #cellName = self.driver.find_element_by_xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[2]").get_attribute("name")
+        
+        cellsNumber = self.driver.find_element_by_xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]").cells
+
+        #self.assertEqual(self.driver.find_element_by_xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[2]").get_attribute("name"), "aHola")
+        import ipdb; ipdb.set_trace()
+        sleep(3)
         
         #import ipdb; ipdb.set_trace()
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(createFolderTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
