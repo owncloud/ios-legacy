3930902eed773ce0532b73540c1b6cd6e848c9bc
Gonzalo González <ggonzalez@solidgear.es>
2015-12-23 18:00:50 +0100

    New class to ManageThumbnails

---
+++ Owncloud iOs Client/FileSystem/ManageThumbnails.h
@@ -0,0 +1,16 @@
+//
+//  ManageThumbnails.h
+//  Owncloud iOs Client
+//
+//  Created by Gonzalo Gonzalez on 23/12/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ManageThumbnails : NSObject
+
++ (id) sharedManager;
+
+
+@end
---
+++ Owncloud iOs Client/FileSystem/ManageThumbnails.m
@@ -0,0 +1,101 @@
+//
+//  ManageThumbnails.m
+//  Owncloud iOs Client
+//
+//  Created by Gonzalo Gonzalez on 23/12/15.
+//
+//
+
+#import "ManageThumbnails.h"
+#import "UtilsUrls.h"
+
+
+static NSString *thumbnailsCacheFolderName = @"thumbnails_cache";
+
+
+@interface ManageThumbnails ()
+
+
+@end
+
+
+@implementation ManageThumbnails
+
+
++ (id)sharedManager {
+    static ManageThumbnails *sharedMyManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedMyManager = [[self alloc] init];
+    });
+    return sharedMyManager;
+}
+
+
+#pragma mark - Store
+
+- (BOOL) storeThumbnail:(NSData *)thumbnail withHash:(NSUInteger) hash {
+    
+   return [[NSFileManager defaultManager] createFileAtPath:[self getThumbnailPathForFileHash:hash] contents:thumbnail attributes:nil];
+    
+}
+
+- (BOOL) isStoredThumbnailWithHash:(NSUInteger) hash {
+    
+    return [[NSFileManager defaultManager] fileExistsAtPath:[self getThumbnailPathForFileHash:hash]];
+    
+}
+
+
+- (BOOL) removeStoredThumbnailWithHash:(NSUInteger) hash {
+    
+    return [[NSFileManager defaultManager] removeItemAtPath:[self getThumbnailPathForFileHash:hash] error:nil];
+}
+
+
+- (BOOL) renameStoredThumbnailWithOldHash:(NSUInteger) oldHash withNewHash:(NSUInteger) newHash {
+    
+    return [[NSFileManager defaultManager] moveItemAtPath:[self getThumbnailPathForFileHash:oldHash] toPath:[self getThumbnailPathForFileHash:newHash] error:nil];
+    
+}
+
+
+#pragma mark - Paths
+
+
+- (NSString *) getThumbnailPathForFileHash:(NSUInteger) hash {
+    
+    return [NSString stringWithFormat:@"%@%ld", [self getThumbnailLocalSystemPath], (long)hash];
+    
+}
+
+
+- (NSString *) getThumbnailLocalSystemPath {
+    
+    return [NSString stringWithFormat:@"%@%@/", [UtilsUrls getOwnCloudFilePath], thumbnailsCacheFolderName];
+    
+}
+
+
+#pragma mark - File System
+
+
+- (void) createThumbnailCacheFolderIfNotExist {
+
+    if (![[NSFileManager defaultManager] fileExistsAtPath:[self getThumbnailLocalSystemPath]]) {
+         NSError *error = nil;
+        [[NSFileManager defaultManager] createDirectoryAtPath:[self getThumbnailLocalSystemPath] withIntermediateDirectories:NO attributes:nil error:&error];
+    }
+}
+
+
+- (void) deleteThumbnailCacheFolder {
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[self getThumbnailLocalSystemPath]]) {
+         NSError *error = nil;
+        [[NSFileManager defaultManager] removeItemAtPath:[self getThumbnailLocalSystemPath] error:&error];
+    }
+    
+}
+
+@end
