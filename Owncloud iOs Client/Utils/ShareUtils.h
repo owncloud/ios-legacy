f2db348cb34a8b2af65b1f076014ca1ea9756c59
Gonzalo González <ggonzalez@solidgear.es>
2016-01-25 09:15:08 +0100

    Added copyright to the new file

--- Owncloud iOs Client/Utils/ShareUtils.h
+++ Owncloud iOs Client/Utils/ShareUtils.h
@@ -4,7 +4,14 @@
 //
 //  Created by Gonzalo Gonzalez on 25/1/16.
 //
-//
+
+/*
+ Copyright (C) 2016, ownCloud, Inc.
+ This code is covered by the GNU Public License Version 3.
+ For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ You should have received a copy of this license
+ along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
 
 #import <Foundation/Foundation.h>
 
