62d438ad395801103f4646d46dc2ab97c7a2ba1e
mRoRo <mrodriguez@solidgear.es>
2016-02-02 12:55:53 +0100

    Removed not necessary code

--- Owncloud iOs Client/Tabs/FileTab/Share/ShareFileOrFolder.m
+++ Owncloud iOs Client/Tabs/FileTab/Share/ShareFileOrFolder.m
@@ -159,22 +159,6 @@
                 [self.activityPopoverController presentPopoverFromRect:CGRectMake(100, 100, 200, 400) inView:_parentView permittedArrowDirections:UIPopoverArrowDirectionLeft animated:YES];
             }
         }
-        
-        [activityView setCompletionHandler:^(NSString *act, BOOL done) {
-            
-//            [self.activityPopoverController dismissPopoverAnimated:YES];
-            
-            /*NSString *serviceMsg = nil;
-             if ( [act isEqualToString:UIActivityTypeMail] )                    ServiceMsg = @"Mail sended!";
-             if ( [act isEqualToString:UIActivityTypePostToTwitter] )           ServiceMsg = @"Post on twitter, ok!";
-             if ( [act isEqualToString:UIActivityTypePostToFacebook] )          ServiceMsg = @"Post on facebook, ok!";
-             if ( [act isEqualToString:UIActivityTypeMessage] )                 ServiceMsg = @"SMS sended!";
-             if ( [act isEqualToString:UIActivityTypeCopyToPasteboard] && done) {
-             serviceMsg = NSLocalizedString(@"link_copied_on_pasteboard", nil);
-             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:serviceMsg message:@"" delegate:nil cancelButtonTitle: NSLocalizedString(@"ok", nil) otherButtonTitles:nil];
-             [alert show];
-             }*/
-        }];
        
     }
   
