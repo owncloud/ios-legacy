33ad462dddb45eec9a3774cca2d96769f1ba743d
mRoRo <mrodriguez@solidgear.es>
2016-04-13 14:29:35 +0200

    change webdriver imports in actions and loginTest files

--- automation_Test/actions.py
+++ automation_Test/actions.py
@@ -8,7 +8,7 @@ import constants as const
 import loginView
 import helpGuideView
 import filesView
-from appium import webdriver
+from /usr/local/bin/appium  import webdriver
 
 def getWebDriver():
     wd = None
