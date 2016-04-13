19dae8d95bc6886d086376f74e158e74357f9e1e
mRoRo <mrodriguez@solidgear.es>
2016-04-13 16:28:33 +0200

    changed installation of Appium

--- automation_Test/actions.py
+++ automation_Test/actions.py
@@ -8,7 +8,7 @@ import constants as const
 import loginView
 import helpGuideView
 import filesView
-from ./usr/local/bin/appium  import webdriver
+from appium import webdriver
 
 def getWebDriver():
     wd = None
