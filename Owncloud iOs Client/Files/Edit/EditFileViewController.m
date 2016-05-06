7b6ee169bbe1cfbe8d7b03b2dc7e034e2a96152a
nasli <nalvarez@solidgear.es>
2016-05-06 15:00:50 +0200

    Clean class and add header

--- Owncloud iOs Client/Files/Edit/EditFileViewController.m
+++ Owncloud iOs Client/Files/Edit/EditFileViewController.m
@@ -6,6 +6,14 @@
 //
 //
 
+/*
+ Copyright (C) 2016, ownCloud, Inc.
+ This code is covered by the GNU Public License Version 3.
+ For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ You should have received a copy of this license
+ along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
+
 #import "EditFileViewController.h"
 #import "FileNameUtils.h"
 #import "ManageUsersDB.h"
@@ -37,7 +45,6 @@
     return self;
 }
 
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
@@ -49,16 +56,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (void) viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
@@ -72,7 +69,6 @@
     
     self.navigationItem.title = NSLocalizedString(@"title_view_edit_file", nil);
     [self setBarButtonStyle];
-    
 }
 
 - (void) setBarButtonStyle {
@@ -82,7 +78,6 @@
     
     UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"cancel", nil) style:UIBarButtonItemStylePlain target:self action:@selector(closeViewController)];
     self.navigationItem.leftBarButtonItem = cancelButton;
-    
 }
 
 
@@ -177,7 +172,6 @@
                                                 contents:fileData
                                               attributes:nil];
     }
-    
 }
 
 
@@ -211,8 +205,7 @@
         [ManageUploadsDB insertUpload:upload];
         [app relaunchUploadsFailedForced];
     }
-    
-    
+
 }
 
 - (void) showAlertView:(NSString*)string{
@@ -220,53 +213,5 @@
     [alertView show];
 }
 
--(void) errorLogin {
-
-}
-
-#pragma mark - Server connect methods
-
-/*
- * Method called when receive a fail from server side
- * @errorCodeFromServer -> WebDav Server Error of NSURLResponse
- * @error -> NSError of NSURLConnection
- */
-
-- (void)manageServerErrors: (NSInteger)errorCodeFromServer and:(NSError *)error {
-    
-//    [self stopPullRefresh];
-//    [self endLoading];
-    
-    AppDelegate *app = (AppDelegate*)[[UIApplication sharedApplication] delegate];
-    [_manageNetworkErrors manageErrorHttp:errorCodeFromServer andErrorConnection:error andUser:app.activeUser];
-}
-
-/*
- * Method that quit the loading screen and unblock the view
- */
-- (void)endLoading {
-    
-//    if (!_isLoadingForNavigate) {
-//        AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-//        //Check if the loading should be visible
-//        if (app.isLoadingVisible==NO) {
-//            // [MBProgressHUD hideAllHUDsForView:self.navigationController.view animated:YES];
-//            [_HUD removeFromSuperview];
-//            self.view.userInteractionEnabled = YES;
-//            self.navigationController.navigationBar.userInteractionEnabled = YES;
-//            self.tabBarController.tabBar.userInteractionEnabled = YES;
-//            [self.view.window setUserInteractionEnabled:YES];
-//        }
-//        
-//        //Check if the app is wainting to show the upload from other app view
-//        if (app.isFileFromOtherAppWaitting && app.isPasscodeVisible == NO) {
-//            [app performSelector:@selector(presentUploadFromOtherApp) withObject:nil afterDelay:0.3];
-//        }
-//        
-//        if (!_rename.renameAlertView.isVisible) {
-//            _rename = nil;
-//        }
-//    }
-}
 
 @end
