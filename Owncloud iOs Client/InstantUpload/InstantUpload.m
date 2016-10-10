96e1744229aa29537f89d85665d0d2fa7e6a1052
Jon-Schneider <jon@jonschneider.me>
2016-10-09 20:50:36 -0400

    Better Instant Upload Timestamp set when activating video or image instnat upload.

--- Owncloud iOs Client/InstantUpload/InstantUpload.m
+++ Owncloud iOs Client/InstantUpload/InstantUpload.m
@@ -81,7 +81,7 @@
                     ACTIVE_USER.imageInstantUpload = YES;
                     [ManageAppSettingsDB updateImageInstantUploadTo:YES];
                     ACTIVE_USER.timestampInstantUpload = [[NSDate date] timeIntervalSince1970];
-                    [ManageAppSettingsDB updateTimestampInstantUpload:[[NSDate date] timeIntervalSince1970]];
+                    [ManageAppSettingsDB updateTimestampInstantUpload:ACTIVE_USER.timestampInstantUpload];
                     [self attemptUpload];
                 } else {
                     [self getMediaLibraryPermission:^(BOOL granted) {
@@ -118,7 +118,7 @@
                     ACTIVE_USER.videoInstantUpload = YES;
                     [ManageAppSettingsDB updateVideoInstantUploadTo:YES];
                     ACTIVE_USER.timestampInstantUpload = [[NSDate date] timeIntervalSince1970];
-                    [ManageAppSettingsDB updateTimestampInstantUpload:[[NSDate date] timeIntervalSince1970]];
+                    [ManageAppSettingsDB updateTimestampInstantUpload:ACTIVE_USER.timestampInstantUpload];
                     [self attemptUpload];
                 } else {
                     [self getMediaLibraryPermission:^(BOOL granted) {
