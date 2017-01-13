bcc182bc8eae17c67cf0b3893819cb41b5deda1d
Gonzalo González <ggonzalez@solidgear.es>
2017-01-13 15:19:35 +0100

    Init the method to store the last version installed in other event

--- Owncloud iOs Client/AppDelegate.m
+++ Owncloud iOs Client/AppDelegate.m
@@ -128,8 +128,6 @@ NSString * NotReachableNetworkForDownloadsNotification = @"NotReachableNetworkFo
     _isExpirationTimeInUpload = NO;
     _isOpenAfterUpgrade = false;
     
-    [UtilsFileSystem  initBundleVersionDefaults];
-    
     [self moveIfIsNecessaryFilesAfterUpdateAppFromTheOldFolderArchitecture];
     
     [self moveIfIsNecessaryFolderOfOwnCloudFromContainerAppSandboxToAppGroupSanbox];
@@ -994,6 +992,9 @@ NSString * NotReachableNetworkForDownloadsNotification = @"NotReachableNetworkFo
     } else {
         DLog(@"Multitasking Not Supported");
     }
+    
+    //Store the version of the app in NSUserDefautls
+    [UtilsFileSystem  initBundleVersionDefaults];
 
 }
 
