093178eeaba7f04e749776675960a48b78aac279
nasli <nalvarez@solidgear.es>
2017-06-26 08:51:03 +0200

    Add new view controler to handle SAML and oauth in webview. Added constants for oauth2

---
+++ Owncloud iOs Client/Login/Login/WebLoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WebLoginViewController.swift
+//  Owncloud iOs Client
+//
+//  Created by Noelia Alvarez on 23/06/2017.
+//
+//
+
+/*
+ Copyright (C) 2017, ownCloud GmbH.
+ This code is covered by the GNU Public License Version 3.
+ For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ You should have received a copy of this license
+ along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
+
+
+import Foundation
+
+
+@objc class WebLoginViewController: UIViewController {
+
+    
+    // MARK: IBOutlets
+    @IBOutlet var webViewLogin: UIWebView!
+
+
+    
+
+}
