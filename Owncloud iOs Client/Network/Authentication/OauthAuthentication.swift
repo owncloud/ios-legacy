be9a891a60d6aaa1a45a1dd035c5cc297a0a85ee
nasli <nalvarez@solidgear.es>
2017-07-06 11:09:34 +0200

    Get access token frome auth code

--- Owncloud iOs Client/Network/Authentication/OauthAuthentication.swift
+++ Owncloud iOs Client/Network/Authentication/OauthAuthentication.swift
@@ -18,23 +18,79 @@
 import Foundation
 
 
-class OauthAuthentication: NSObject {
+class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate {
 
 
-    func authCodeRequest(_ urlString: String, withCompletion completion: @escaping (_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
+    func accessTokenAuthRequest(_ url: URL, authCode: String, withCompletion completion: @escaping (_ data: NSData?,_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
      
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        request.setValue(UtilsUrls.getUserAgent(), forHTTPHeaderField: "User-Agent")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        let authId = k_oauth2_client_id+":"+k_oauth2_client_secret
+        let base64EncodedAuthId: String = UtilsFramework.afBase64EncodedString(from: authId)
+        request.setValue("Basic \(base64EncodedAuthId)", forHTTPHeaderField: "Authorization")
 
-
+        let body =  "grant_type=authorization_code&code=\(authCode)&redirect_uri=\(k_oauth2_redirect_uri)&client_id=\(k_oauth2_client_id)"
+        let bodyEncoded = body.data(using: String.Encoding.utf8)
+        request.httpBody = bodyEncoded
+        print("request body: \(bodyEncoded))")
+        let configuration = URLSessionConfiguration.ephemeral
+        let session = URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
+        //        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil, nil, error)
+                
+            } else if let data = data {
+                do {
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: AnyObject] {
+                        print("accessTokenAuthRequest json:", json)
+                        completion(data as NSData, response as? HTTPURLResponse, error)
+                    }
+                    
+                } catch let error {
+                    print("accessTokenAuthRequest  no data error:", error.localizedDescription)
+                    completion(nil, response as? HTTPURLResponse, error)
+                }
+                
+            }
+        })
+        task.resume()
     }
     
-    func oauthUrlTogetAuthCodeFrom (serverPath : String) -> URL {
+    func getAuthDataBy(url: URL, authCode: String, withCompletion completion: @escaping (_ authData: NSData? ,_ error: Error?) -> Void)  {
+        
+        self.accessTokenAuthRequest(url, authCode: authCode) { (data:NSData?, httpResponse: HTTPURLResponse?,error: Error?) in
+            
+            //TODO: return data in format
+            if (data != nil) {
+                completion(data, nil )
+            } else {
+                completion(nil, error)
+            }
+        }
+        
+    }
+    
+    func storeOauthData () {
+        
+    }
+    
+    
+    func oauthUrlTogetAuthCodeWith (serverPath : String) -> URL {
         
         let oauth2RedirectUri = k_oauth2_redirect_uri
         let oauth2RedirectUriEncoded = oauth2RedirectUri.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlHostAllowed)
         
-        
-        let urlComps = NSURLComponents(string: serverPath)!
-        urlComps.path = k_oauth2_authorization_endpoint
+        let fullServerPath = serverPath + k_oauth2_authorization_endpoint
+        let urlComps: NSURLComponents = NSURLComponents(string: fullServerPath)!
         
         let queryItems = [NSURLQueryItem(name: "response_type", value: "code"),
                           NSURLQueryItem(name: "redirect_uri", value: oauth2RedirectUriEncoded),
@@ -42,14 +98,22 @@ class OauthAuthentication: NSObject {
                         ]
         urlComps.queryItems = queryItems as [URLQueryItem]
         
-        let fullOauthUrl = urlComps.url!
+        let fullOauthUrl = urlComps.url
         
-        return fullOauthUrl
+        return fullOauthUrl!
         
     }
+
+    func oauthUrlToGetTokenWith(serverPath : String) -> URL {
     
-    
+        var serverPathUrl = URL(string: serverPath)
+        serverPathUrl = serverPathUrl?.appendingPathComponent(k_oauth2_token_endpoint)
+        let urlComps = NSURLComponents(string: (serverPathUrl?.absoluteString)!)
+        
+        let fullOauthUrl = urlComps?.url!
+        
+        return fullOauthUrl!
+    }
 
     
-
 }
