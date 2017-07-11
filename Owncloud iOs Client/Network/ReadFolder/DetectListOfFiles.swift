6a60c28fa1e1852266e7a4e86acc9c0294a797c6
nasli <nalvarez@solidgear.es>
2017-07-11 19:20:26 +0200

    Handle flow get list of files

--- Owncloud iOs Client/Network/ReadFolder/DetectListOfFiles.swift
+++ Owncloud iOs Client/Network/ReadFolder/DetectListOfFiles.swift
@@ -19,51 +19,60 @@ import Foundation
 
 @objc class DetectListOfFiles: NSObject {
     
-//    func readFolderRequest(_ url: URL, authType: AuthenticationMethod, username: String?, accessToken: String, withCompletion completion: @escaping (_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
-//        
-//    //    var sharedCommunication = AppDelegate.sharedOCCommunication()
-//        
-//      //  self.setCredencialsAndUserAgentWith(sharedOCCommunication: sharedCommunication!, authType: authType, username: username, accessToken: accessToken)
-//        
-//        //request
-//        
-//        
-//        
-//        //return
-//        
-//     
-//    }
-    
-    
-    func setCredencialsAndUserAgentWith(sharedOCCommunication: OCCommunication, authType: AuthenticationMethod, username: String?, accessToken: String) {
+    func readFolderRequest(_ url: URL, authType: AuthenticationMethod, username: String?, accessToken: String, withCompletion completion: @escaping (_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
         
-        sharedOCCommunication.setValueOfUserAgent(UtilsUrls.getUserAgent())
+        //var sharedCommunication = AppDelegate.sharedOCCommunication()
+        
+        self.setCredencialsAndUserAgentWith(authType: authType, username: username, accessToken: accessToken)
+        
+        
+        AppDelegate.sharedOCCommunication().readFolder(url.absoluteString, withUserSessionToken: accessToken, on: AppDelegate.sharedOCCommunication(),
+            
+           successRequest: { (response: HTTPURLResponse?, items: [Any]?, redirectedServer: String?, token: String?) in
+            
+            completion(response,nil) //TODO
+            
+                                                        
+            
+        }, failureRequest: { (response:HTTPURLResponse?, error: Error?, token: String?, redirectedServer: String?) in
+            //handle errors
+            
+            completion(response,error) //TODO
+            
+            
+        })
+    }
+    
+    
+    func setCredencialsAndUserAgentWith(authType: AuthenticationMethod, username: String?, accessToken: String) {
+        
+        AppDelegate.sharedOCCommunication().setValueOfUserAgent(UtilsUrls.getUserAgent())
 
         switch authType {
         case .BEARER_TOKEN:
-            sharedOCCommunication.setCredentialsOauthWithToken(accessToken)
+            AppDelegate.sharedOCCommunication().setCredentialsOauthWithToken(accessToken)
             break
         case .SAML_WEB_SSO:
             if (Customization.kIsSsoActive()) {
-                sharedOCCommunication.setCredentialsWithCookie(accessToken)
+                AppDelegate.sharedOCCommunication().setCredentialsWithCookie(accessToken)
                 break
             }
         default:
-            sharedOCCommunication.setCredentialsWithUser(username, andPassword: accessToken)
+            AppDelegate.sharedOCCommunication().setCredentialsWithUser(username, andPassword: accessToken)
             break
         }
     }
     
     
-//    func getListOfFiles(url:URL, authType: AuthenticationMethod, accessToken: String) -> Array<Any> {
-//        
+    func getListOfFiles(url:URL, authType: AuthenticationMethod, accessToken: String) -> Array<Any> {
+        
 //        self.readFolderRequest(url, authType: authType, username: nil, accessToken: accessToken) { (httpResponse: HTTPURLResponse?, error: Error?) in
 //        
 //
 //        }
-//        var resultArray = [""]
-//        
-//        return resultArray
-//    }
+        var items: Array = [""]
+        
+        return items
+    }
     
 }
