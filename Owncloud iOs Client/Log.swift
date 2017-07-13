0d576c5e23b4e31ed296ed08e4c15499bcc4cba9
nasli <nalvarez@solidgear.es>
2017-07-13 10:36:03 +0200

    Refactor getListOfFiles after new login flow finished. Add support to manage errors in new login view

---
+++ Owncloud iOs Client/Log.swift
@@ -0,0 +1,40 @@
+//
+//  Log.swift
+//  Owncloud iOs Client
+//
+//  Created by Noelia Alvarez on 12/07/2017.
+//
+//
+
+/*
+ Copyright (C) 2017, ownCloud GmbH.
+ This code is covered by the GNU Public License Version 3.
+ For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
+ You should have received a copy of this license
+ along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
+ */
+
+
+import Foundation
+
+
+//class Log {
+//    
+//    var intFor : Int
+//    
+//    init() {
+//        intFor = 42
+//    }
+//    
+//    func DLog(message: String, function: String = #function) {
+//        
+//        #if DEBUG_MODE
+//            let lastPathOfFile = (#file as NSString).lastPathComponent
+//            print("<\(self) \(lastPathOfFile):(\(#line)> (\(message)), \(##__VA_ARGS__)" )
+//        #else
+//            print("\(message)")
+//        #endif
+//    }
+//}
+
+
