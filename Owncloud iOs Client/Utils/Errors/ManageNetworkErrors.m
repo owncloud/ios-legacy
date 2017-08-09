87fd4ff3b9e269e6016491700af77a9ed7df8fa3
David A. Velasco <dvelasco@solidgear.es>
2017-08-09 19:11:50 +0200

    Grant that erros on validation of credentials are shown for all the paths of failure

--- Owncloud iOs Client/Utils/Errors/ManageNetworkErrors.m
+++ Owncloud iOs Client/Utils/Errors/ManageNetworkErrors.m
@@ -103,4 +103,104 @@
     }
 }
 
+
+/**
+ * Returns an error message corresponding to a remote operation result with no knowledge about
+ * the operation performed.
+ *
+ * @param result        Result of a remote operation performed.
+ * @return User message corresponding to 'result'.
+ */
+/*
+- (NSString *) getCommonMessageForResult:(NSInteger) errorHttp andError:(NSError *) error {
+    
+    NSString* message = nil;
+    
+    if (error.code == ) {
+        message = res.getString(R.string.network_error_socket_exception);
+    
+    } else if (result.getCode() == ResultCode.NO_NETWORK_CONNECTION) {
+        message = res.getString(R.string.error_no_network_connection);
+    
+    } else if (result.getCode() == ResultCode.TIMEOUT) {
+        message = res.getString(R.string.network_error_socket_timeout_exception);
+        
+        if (result.getException() instanceof ConnectTimeoutException) {
+            message = res.getString(R.string.network_error_connect_timeout_exception);
+        }
+            
+    } else if (result.getCode() == ResultCode.HOST_NOT_AVAILABLE) {
+        message = res.getString(R.string.network_host_not_available);
+            
+    } else if (result.getCode() == ResultCode.SERVICE_UNAVAILABLE) {
+        message = res.getString(R.string.service_unavailable);
+            
+    } else if (result.getCode() == ResultCode.SSL_RECOVERABLE_PEER_UNVERIFIED) {
+        message = res.getString(
+            R.string.ssl_certificate_not_trusted
+        );
+            
+    } else if (result.getCode() == ResultCode.BAD_OC_VERSION) {
+        message = res.getString(
+            R.string.auth_bad_oc_version_title
+        );
+            
+    } else if (result.getCode() == ResultCode.INCORRECT_ADDRESS) {
+        message = res.getString(
+            R.string.auth_incorrect_address_title
+        );
+            
+    } else if (result.getCode() == ResultCode.SSL_ERROR) {
+        message = res.getString(
+            R.string.auth_ssl_general_error_title
+        );
+            
+    } else if (result.getCode() == ResultCode.UNAUTHORIZED) {
+        message = res.getString(
+            R.string.auth_unauthorized
+        );
+        
+    } else if (result.getCode() == ResultCode.INSTANCE_NOT_CONFIGURED) {
+        message = res.getString(
+            R.string.auth_not_configured_title
+        );
+            
+    } else if (result.getCode() == ResultCode.FILE_NOT_FOUND) {
+        message = res.getString(
+            R.string.auth_incorrect_path_title
+        );
+        
+    } else if (result.getCode() == ResultCode.OAUTH2_ERROR) {
+        message = res.getString(
+            R.string.auth_oauth_error
+        );
+            
+    } else if (result.getCode() == ResultCode.OAUTH2_ERROR_ACCESS_DENIED) {
+        message = res.getString(
+            R.string.auth_oauth_error_access_denied
+        );
+            
+    } else if (result.getCode() == ResultCode.ACCOUNT_NOT_NEW) {
+        message = res.getString(
+            R.string.auth_account_not_new
+        );
+            
+    } else if (result.getCode() == ResultCode.ACCOUNT_NOT_THE_SAME) {
+        message = res.getString(
+            R.string.auth_account_not_the_same
+        );
+            
+    } else if (result.getCode() == ResultCode.OK_REDIRECT_TO_NON_SECURE_CONNECTION) {
+        message = res.getString(R.string.auth_redirect_non_secure_connection_title);
+    }
+        
+    else if (result.getHttpPhrase() != null && result.getHttpPhrase().length() > 0) {
+        // last chance: error message from server
+        message = result.getHttpPhrase();
+    }
+
+    return message;
+}
+*/
+
 @end
