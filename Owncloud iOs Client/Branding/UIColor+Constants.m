520e657fcef114d81f7a4642614e3f5ebcb5306b
nasli <nalvarez@solidgear.es>
2017-08-16 10:34:00 +0200

    Update login background color to white

--- Owncloud iOs Client/Branding/UIColor+Constants.m
+++ Owncloud iOs Client/Branding/UIColor+Constants.m
@@ -96,7 +96,7 @@
 
 //Background color of login view
 + (UIColor*)colorOfLoginBackground{
-    return [UIColor colorWithRed:245/255.0f green:245/255.0f blue:241/255.0f alpha:1.0f];
+    return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0f];
 }
 
 //Text color of url in login view
