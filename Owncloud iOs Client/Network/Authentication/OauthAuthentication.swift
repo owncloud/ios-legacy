bd381835be8fdbfcc96b2da0d7603f3e38f3a094
nasli <nalvarez@solidgear.es>
2017-08-25 13:56:12 +0200

    Update oauthauthentication class to use it within objective classes

--- Owncloud iOs Client/Network/Authentication/OauthAuthentication.swift
+++ Owncloud iOs Client/Network/Authentication/OauthAuthentication.swift
@@ -18,11 +18,11 @@
 import Foundation
 
 
-class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate {
+@objc class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate {
     
 // MARK : get authData by authcode
 
-    func accessTokenAuthRequest(_ url: URL, authCode: String, withCompletion completion: @escaping (_ data: Data?,_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
+@objc func accessTokenAuthRequest(_ url: URL, authCode: String, withCompletion completion: @escaping (_ data: Data?,_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
      
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -57,8 +57,8 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
         task.resume()
     }
     
-    
-    func getAuthDataBy(url: URL, authCode: String, withCompletion completion: @escaping (_ userCredDto: OCCredentialsDto? ,_ error: Error?) -> Void)  {
+
+@objc    func getAuthDataBy(url: URL, authCode: String, withCompletion completion: @escaping (_ userCredDto: OCCredentialsDto? ,_ error: String?) -> Void)  {
         
         self.accessTokenAuthRequest(url, authCode: authCode, withCompletion: { (data:Data?, httpResponse:HTTPURLResponse?, error:Error?) in
             
@@ -104,7 +104,7 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
     }
     
     
-    func oauthUrlTogetAuthCodeWith (serverPath : String) -> URL {
+@objc    func oauthUrlTogetAuthCodeWith (serverPath : String) -> URL {
         
         let oauth2RedirectUri = k_oauth2_redirect_uri
         let oauth2RedirectUriEncoded = oauth2RedirectUri.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlHostAllowed)
@@ -124,7 +124,7 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
         
     }
 
-    func oauthUrlToGetTokenWith(serverPath : String) -> URL {
+@objc    func oauthUrlToGetTokenWith(serverPath : String) -> URL {
     
         var serverPathUrl = URL(string: serverPath)
         serverPathUrl = serverPathUrl?.appendingPathComponent(k_oauth2_token_endpoint)
@@ -139,7 +139,7 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
     
     // MARK : get authData by refreshToken
     
-    func refreshTokenAuthRequest(_ url: URL, refreshToken: String, withCompletion completion: @escaping (_ data: Data?,_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
+@objc    func refreshTokenAuthRequest(_ url: URL, refreshToken: String, withCompletion completion: @escaping (_ data: Data?,_ httpResponse: HTTPURLResponse?, _ error: Error?) -> Void) {
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -176,7 +176,7 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
     }
     
     
-    func getAuthDataBy(url: URL, refreshToken: String, withCompletion completion: @escaping (_ userCredDto: OCCredentialsDto? ,_ error: String?) -> Void)  {
+@objc    func getAuthDataBy(url: URL, refreshToken: String, withCompletion completion: @escaping (_ userCredDto: OCCredentialsDto? ,_ error: String?) -> Void)  {
         
         self.refreshTokenAuthRequest(url, refreshToken:refreshToken, withCompletion: { (data:Data?, httpResponse:HTTPURLResponse?, error:Error?) in
             
@@ -213,13 +213,6 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
             }
         })
     }
-    
-    
-    func isAccessTokenDateExpired(_ tokenDate: String?) ->Bool {
-        //TODO: compare dates
-        return true
-    }
-    
 
 /// MARK : methods from URLSessionDelegate
     
