9bd8fbf7db22ae9b067a2370619958583d945ab4
David A. Velasco <dvelasco@solidgear.es>
2017-08-31 10:14:57 +0200

    Add check of user-accepted unsecure certificates to OAuth2 requests

--- Owncloud iOs Client/Network/Authentication/OauthAuthentication.swift
+++ Owncloud iOs Client/Network/Authentication/OauthAuthentication.swift
@@ -40,7 +40,6 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
         
         let configuration = URLSessionConfiguration.ephemeral
         let session = URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
-        //        let session = URLSession(configuration: URLSessionConfiguration.default)
         
         let task = session.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
             
@@ -221,11 +220,37 @@ class OauthAuthentication: NSObject, URLSessionDelegate, URLSessionTaskDelegate 
         return true
     }
     
-//    if self.isAccessTokenDateExpired(self.user?.credDto.expiresIn) {
-//    
-//    } else {
-//    
-//    }
 
+/// MARK : methods from URLSessionDelegate
+    
+    // Delegate method called when the server responded with an authentication challenge.
+    // Since iOS is so great, it is also called when the server certificate is not trusted, so that the client
+    // can decide what to do about it.
+    //
+    // In this case we only expect to receive an authentication challenge if the server holds a certificate signed
+    // by an authority that is not trusted by iOS system. In this case, we need to check the list of certificates
+    // that were explicitly accepted by the user before, and allow the request to go on if the current one matches
+    // one of them (and not in other case).
+    //
+    func urlSession(_ session: URLSession,
+                    didReceive challenge: URLAuthenticationChallenge,
+                    completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Swift.Void) {
+
+        // For the case that the call is due to server certificate not trusted by iOS, we compare the certificate in the
+        // authentication challenge with the certificates that were previously accepted by the user in the OC app. If
+        // it match any of them, we allow to go on.
+        let sslCertificateManager: SSLCertificateManager = SSLCertificateManager()
+        let trusted: Bool = sslCertificateManager.isTrustedServerCertificate(in: challenge)
+        if (trusted) {
+            let credential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+            completionHandler(.useCredential,credential);
+            
+        } else {
+            // If this method was called due to an unstrusted server certificate and this was not accepted by the user before,
+            // or if it was called due to a different authentication challenge, default handling will lead the task to fail.
+            completionHandler(.performDefaultHandling, nil);
+        }
+
+    }
     
 }
--- Owncloud iOs Client/Utils/SSL/CheckAccessToServer.m
+++ Owncloud iOs Client/Utils/SSL/CheckAccessToServer.m
@@ -33,8 +33,6 @@
 #import "AppDelegate.h"
 #endif
 
-static NSString *const tmpFileName = @"tmp.der";
-
 @implementation CheckAccessToServer
 
 
@@ -47,27 +45,10 @@ static NSString *const tmpFileName = @"tmp.der";
     dispatch_once(&onceToken, ^{
         sharedCheckAccessToServer = [[self alloc] init];
         sharedCheckAccessToServer.sslStatus = sslStatusNotChecked;
+        sharedCheckAccessToServer.sslCertificateManager = [[SSLCertificateManager alloc] init];
     });
     return sharedCheckAccessToServer;
 }
-
-static SecCertificateRef SecTrustGetLeafCertificate(SecTrustRef trust)
-// Returns the leaf certificate from a SecTrust object (that is always the 
-// certificate at index 0).
-{
-    SecCertificateRef   result;
-    
-    assert(trust != NULL);
-    
-    if (SecTrustGetCertificateCount(trust) > 0) {
-        result = SecTrustGetCertificateAtIndex(trust, 0);
-        assert(result != NULL);
-    } else {
-        result = NULL;
-    }
-    return result;
-}
-
 
 -(void) isConnectionToTheServerByUrl:(NSString *) url {
     [self isConnectionToTheServerByUrl:url withTimeout:k_timeout_webdav];
@@ -231,92 +212,14 @@ static SecCertificateRef SecTrustGetLeafCertificate(SecTrustRef trust)
 }
 
 - (BOOL) isTrustedServerWithChallenge:(NSURLAuthenticationChallenge *)challenge {
-    BOOL trusted = NO;
-    SecTrustRef trust;
-    NSURLProtectionSpace *protectionSpace;
-    
-    protectionSpace = [challenge protectionSpace];
-    trust = [protectionSpace serverTrust];
-    
-    [self createFolderToSaveCertificates];
-    
-    if(trust != nil) {
-        [self saveCertificate:trust withName:tmpFileName];
-        
-        NSString *localCertificatesFolder = [UtilsUrls getLocalCertificatesPath];
-        
-        NSMutableArray *listCertificateLocation = [ManageAppSettingsDB getAllCertificatesLocation];
-        
-        for (int i = 0 ; i < [listCertificateLocation count] ; i++) {
-            
-            NSString *currentLocalCertLocation = [listCertificateLocation objectAtIndex:i];
-            NSFileManager *fileManager = [ NSFileManager defaultManager];
-            if([fileManager contentsEqualAtPath:[NSString stringWithFormat:@"%@%@",localCertificatesFolder,tmpFileName] andPath:[NSString stringWithFormat:@"%@",currentLocalCertLocation]]) {
-                DLog(@"Is the same certificate!!!");
-                self.isSameCertificateSelfSigned = YES;
-                trusted = YES;
-            }
-        }
-    } else {
-        trusted = NO;
-    }
-
+    BOOL trusted = [self.sslCertificateManager isTrustedServerCertificateIn:challenge];
+    self.isSameCertificateSelfSigned = trusted;
     return trusted;
 }
 
 
 - (void)saveCertificate:(SecTrustRef) trust withName:(NSString *) certName {
-    SecCertificateRef currentServerCert = SecTrustGetLeafCertificate(trust);
-    
-    CFDataRef data = SecCertificateCopyData(currentServerCert);
-    X509 *x509cert = NULL;
-    if (data) {
-        BIO *mem = BIO_new_mem_buf((void *)CFDataGetBytePtr(data), (int)CFDataGetLength(data));
-        x509cert = d2i_X509_bio(mem, NULL);
-        BIO_free(mem);
-        CFRelease(data);
-        
-        if (!x509cert) {
-            DLog(@"OpenSSL couldn't parse X509 Certificate");
-            
-        } else {
-            
-            NSString *localCertificatesFolder = [UtilsUrls getLocalCertificatesPath];
-            
-            certName = [NSString stringWithFormat:@"%@%@",localCertificatesFolder,certName];
-            
-            if ([[NSFileManager defaultManager] fileExistsAtPath:certName]) {
-                NSError *error;
-                [[NSFileManager defaultManager] removeItemAtPath:certName error:&error];
-            }
-            
-            FILE *file;
-            file = fopen( [certName UTF8String], "w" );
-            if (file) {
-                PEM_write_X509(file, x509cert);
-            }
-            fclose(file);
-
-        }
-    
-    } else {
-        DLog(@"Failed to retrieve DER data from Certificate Ref");
-    }
-    //Free
-    X509_free(x509cert);
-}
-
-- (void)createFolderToSaveCertificates {
-    NSString *documentsDirectory = [UtilsUrls getOwnCloudFilePath]; // Get documents folder
-    NSString *dataPath = [documentsDirectory stringByAppendingPathComponent:@"Certificates"];
-    
-    if (![[NSFileManager defaultManager] fileExistsAtPath:dataPath]) {
-        NSError *error = nil;
-        
-        [[NSFileManager defaultManager] createDirectoryAtPath:dataPath withIntermediateDirectories:NO attributes:nil error:&error];
-        
-        DLog(@"Error: %@", [error localizedDescription]);
-    }
+    [self.sslCertificateManager saveCertificate:trust withName:certName];
 }
 
 /*
@@ -365,26 +268,7 @@ static SecCertificateRef SecTrustGetLeafCertificate(SecTrustRef trust)
 
 - (void) acceptCertificate {
     DLog(@"user pressed YES");
-    //Save temporal certificate
-    
-    NSString *localCertificatesFolder = [UtilsUrls getLocalCertificatesPath];
-    
-    NSError * err = NULL;
-    NSFileManager * fm = [[NSFileManager alloc] init];
-    
-    NSDate *date = [NSDate date];
-    NSString *currentCertLocation = [NSString stringWithFormat:@"%@%f.der",localCertificatesFolder, [date timeIntervalSince1970]];
-    
-    DLog(@"currentCertLocation: %@", currentCertLocation);
-    
-    BOOL result = [fm moveItemAtPath:[NSString stringWithFormat:@"%@%@",localCertificatesFolder, tmpFileName] toPath:currentCertLocation error:&err];
-    if(!result) {
-        DLog(@"Error: %@", [err localizedDescription]);
-    } else {
-        [ManageAppSettingsDB insertCertificate:[NSString stringWithFormat:@"%f.der", [date timeIntervalSince1970]]];
-        
-    }
-
+    [self.sslCertificateManager acceptCurrentCertificate];
 }
 
 - (void)URLSession:(NSURLSession *)session
@@ -450,24 +334,8 @@ willPerformHTTPRedirection:(NSHTTPURLResponse *)redirectResponse
 
 
 - (BOOL) isTemporalCertificateTrusted {
-    
-    BOOL trusted = NO;
-    
-    NSString *localCertificatesFolder = [UtilsUrls getLocalCertificatesPath];
-    
-    NSMutableArray *listCertificateLocation = [ManageAppSettingsDB getAllCertificatesLocation];
-    
-    for (int i = 0 ; i < [listCertificateLocation count] ; i++) {
-        
-        NSString *currentLocalCertLocation = [listCertificateLocation objectAtIndex:i];
-        NSFileManager *fileManager = [ NSFileManager defaultManager];
-        if([fileManager contentsEqualAtPath:[NSString stringWithFormat:@"%@%@",localCertificatesFolder,tmpFileName] andPath:[NSString stringWithFormat:@"%@",currentLocalCertLocation]]) {
-            DLog(@"Is the same certificate!!!");
-            trusted = YES;
-            self.isSameCertificateSelfSigned = YES;
-        }
-    }
-    
+    BOOL trusted = [self.sslCertificateManager isCurrentCertificateTrusted];
+    self.isSameCertificateSelfSigned = trusted;
     return trusted;
 }
 
