511dec4083604936da37622c4fd2e6d18b00a0b7
nasli <nalvarez@solidgear.es>
2017-11-07 17:27:10 +0100

    Remove not needed code in account cell

--- Owncloud iOs Client/Tabs/SettingTab/Account/Cell/AccountCell.m
+++ Owncloud iOs Client/Tabs/SettingTab/Account/Cell/AccountCell.m
@@ -6,25 +6,17 @@
 //
 
 /*
- Copyright (C) 2016, ownCloud GmbH.
+ Copyright (C) 2017, ownCloud GmbH.
  This code is covered by the GNU Public License Version 3.
  For distribution utilizing Apple mechanisms please see https://owncloud.org/contribute/iOS-license-exception/
  You should have received a copy of this license
  along with this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.en.html>.
  */
 
-#import "AppDelegate.h"
 #import "AccountCell.h"
-#import "CheckAccessToServer.h"
-#import "UtilsFramework.h"
-#import "ManageUsersDB.h"
-#import "Customization.h"
-#import "OCKeychain.h"
-#import "UtilsCookies.h"
 
 @implementation AccountCell
 
-//TODO: use autolayout for cell 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
