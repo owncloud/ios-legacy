blissbless/ios

Fix crash after switch between account

Owncloud iOs Client/AppDelegate.h
@@ -295,7 +295,7 @@ extern NSString * NotReachableNetworkForDownloadsNotification;
  * For iPad:
  *    - The same TabBarController with three items.
  *    - Detail View.
- *
+ * 
  */
 - (void) generateAppInterfaceFromLoginScreen:(BOOL)isFromLogin;
 
@@ -331,6 +331,6 @@ extern NSString * NotReachableNetworkForDownloadsNotification;
  *
  * @param user -> UserDto to set as active user
  */
-- (void) switchActiveUserTo:(UserDto *) user inHardMode:(BOOL)hardMode withCompletionHandler:(void (^)(void)) completionHandler;
+- (void) switchActiveUserTo:(UserDto *) user inHardMode:(BOOL)hardMode;
 
 @end

Owncloud iOs Client/AppDelegate.m
@@ -2780,12 +2780,9 @@ float shortDelay = 0.3;
 }
 
 
-#pragma mark - Active User
+#pragma mark - Switch Active User
 
-- (void) switchActiveUserTo:(UserDto *)user inHardMode:(BOOL)hardMode withCompletionHandler:(void (^)(void)) completionHandler {
-    
-    // all the switch is performed in background, without blocking the caller thread, that should be main
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+- (void) switchActiveUserTo:(UserDto *)user inHardMode:(BOOL)hardMode {
     
         self.userSessionCurrentToken = nil;
             // should be here or right after checking the user really changed? for the moment, here
@@ -2797,9 +2794,9 @@ float shortDelay = 0.3;
                 [UtilsCookies eraseCredentialsAndUrlCacheOfActiveUser];
             }
         
-            // Cancel downloads of the previous user, in the same background thread
+            // Cancel downloads of the previous user
             [self portedCancelAllDownloads];
-        
+            
             // update active state of users in DB
             [ManageUsersDB setAllUsersNoActive];
             [ManageUsersDB setActiveAccountByUserId:user.userId];
@@ -2823,29 +2820,24 @@ float shortDelay = 0.3;
             [UtilsCookies eraseURLCache];
         
             //we create the user folder to haver multiuser
-            [UtilsFileSystem createFolderForUser:APP_DELEGATE.activeUser];
+            [UtilsFileSystem createFolderForUser:user];
             
             self.isNewUser = YES;
             
             ManageAccounts *manageAccounts = [ManageAccounts new];
-            [manageAccounts updateDisplayNameOfUserWithUser:self.activeUser];
+            [manageAccounts updateDisplayNameOfUserWithUser:user];
         }
-    
-        // completion handler is called in main thread
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (completionHandler) {
-                completionHandler();
-            }
-        });
-    });
 }
 
 
 - (void) portedCancelAllDownloads {
     //Cancel downloads in ipad
-    [self.downloadManager cancelDownloads];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+
+        [self.downloadManager cancelDownloads];
     
-    [[AppDelegate sharedSyncFolderManager] cancelAllDownloads];
+        [[AppDelegate sharedSyncFolderManager] cancelAllDownloads];
+    });
 }
 
 

Owncloud iOs Client/Login/Login/UniversalLoginViewController.swift
@@ -893,10 +893,9 @@ public enum TextfieldType: String {
                             if self.user != nil {
                                 ManageFiles().storeListOfFiles(listOfFileDtos!, forFileId: 0, andUser: self.user!)
                             
-                                app.switchActiveUser(to: self.user, inHardMode: true, withCompletionHandler:
-                                    {
-                                    app.generateAppInterface(fromLoginScreen: true)
-                                })
+                                app.switchActiveUser(to: self.user, inHardMode: true)
+                                app.generateAppInterface(fromLoginScreen: true)
+                                
                             } else {
                                 self.showURLError(NSLocalizedString("error_could_not_add_account", comment: ""))
                             }
@@ -912,7 +911,7 @@ public enum TextfieldType: String {
                         if self.loginMode == .migrate {
                             // migration mode needs to start a fresh list of files, so that it is updated with the new URL
                             app.generateAppInterface(fromLoginScreen: true)
-                            
+                              
                         } else {
                             self.closeLoginView()
                         }

Owncloud iOs Client/Tabs/SettingTab/SettingsViewController.m
@@ -791,8 +791,7 @@
     static NSString *CellIdentifier = @"AccountCell";
     
     AccountCell *accountCell = (AccountCell *) [self.settingsTableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    UserDto *userAccount = [UserDto new];
-    userAccount = [self.listUsers objectAtIndex:row];
+    UserDto *userAccount = [self.listUsers objectAtIndex:row];
     
     if (accountCell == nil) {
         
@@ -1104,24 +1103,20 @@
     //We check the connection here because we need to accept the certificate on the self signed server before go to the files tab
     [[CheckAccessToServer sharedManager] isConnectionToTheServerByUrl:[UtilsUrls getFullRemoteServerPath:selectedUser]];
 
-    
     //Method to change the account
     [[OCLoadingSpinner sharedOCLoadingSpinner] initLoadingForViewController: self];
-    [app switchActiveUserTo:selectedUser inHardMode:NO withCompletionHandler:^{
-        DLog(@"refreshing list of accounts after user was switched");
+    [app switchActiveUserTo:selectedUser inHardMode:NO];
+    DLog(@"refreshing list of accounts after user was switched");
         
-        [[OCLoadingSpinner sharedOCLoadingSpinner] endLoading];
+    [[OCLoadingSpinner sharedOCLoadingSpinner] endLoading];
         
-        //If ipad, clean the detail view
-        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-            AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication]delegate];
-            [app presentWithView];
-        }
-                
-        [self refreshTable];
-        
-    } ];
+    //If ipad, clean the detail view
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+        AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+        [app presentWithView];
+    }
     
+    [self refreshTable];
 }
 
 #pragma mark - AddAccountDelegate

owncloud/ios-legacy

Owncloud iOs Client/AppDelegate.h
@@ -295,7 +295,7 @@ extern NSString * NotReachableNetworkForDownloadsNotification;
  * For iPad:
  *    - The same TabBarController with three items.
  *    - Detail View.
- *
+ * 
  */
 - (void) generateAppInterfaceFromLoginScreen:(BOOL)isFromLogin;
 
@@ -332,6 +332,6 @@ extern NSString * NotReachableNetworkForDownloadsNotification;
  * @param user -> UserDto to set as active user
  * @param isNewAccount -> BOOL if this user is a new account added
  */
-- (void) switchActiveUserTo:(UserDto *)user isNewAccount:(BOOL)isNewAccount withCompletionHandler:(void (^)(void)) completionHandler;
+- (void) switchActiveUserTo:(UserDto *) user isNewAccount:(BOOL)isNewAccount;
 
 @end

Owncloud iOs Client/AppDelegate.m
@@ -2787,12 +2787,9 @@ float shortDelay = 0.3;
 }
 
 
-#pragma mark - Active User
+#pragma mark - Switch Active User
 
-- (void) switchActiveUserTo:(UserDto *)user isNewAccount:(BOOL)isNewAccount withCompletionHandler:(void (^)(void)) completionHandler {
-    
-    // all the switch is performed in background, without blocking the caller thread, that should be main
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+- (void) switchActiveUserTo:(UserDto *)user isNewAccount:(BOOL)isNewAccount {
     
         self.userSessionCurrentToken = nil;
             // should be here or right after checking the user really changed? for the moment, here
@@ -2808,9 +2805,9 @@ float shortDelay = 0.3;
                 [UtilsCookies eraseCredentialsAndUrlCacheOfActiveUser];
             }
         
-            // Cancel downloads of the previous user, in the same background thread
+            // Cancel downloads of the previous user
             [self portedCancelAllDownloads];
-        
+            
             // update active state of users in DB
             [ManageUsersDB setAllUsersNoActive];
             [ManageUsersDB setActiveAccountByUserId:user.userId];
@@ -2826,29 +2823,24 @@ float shortDelay = 0.3;
             [UtilsCookies eraseURLCache];
         
             //we create the user folder to haver multiuser
-            [UtilsFileSystem createFolderForUser:APP_DELEGATE.activeUser];
+            [UtilsFileSystem createFolderForUser:user];
             
             self.isNewUser = YES;
             
             ManageAccounts *manageAccounts = [ManageAccounts new];
-            [manageAccounts updateDisplayNameOfUserWithUser:self.activeUser];
+            [manageAccounts updateDisplayNameOfUserWithUser:user];
         }
-    
-        // completion handler is called in main thread
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (completionHandler) {
-                completionHandler();
-            }
-        });
-    });
 }
 
 
 - (void) portedCancelAllDownloads {
     //Cancel downloads in ipad
-    [self.downloadManager cancelDownloads];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+
+        [self.downloadManager cancelDownloads];
     
-    [[AppDelegate sharedSyncFolderManager] cancelAllDownloads];
+        [[AppDelegate sharedSyncFolderManager] cancelAllDownloads];
+    });
 }
 
 

Owncloud iOs Client/Login/Login/UniversalLoginViewController.swift
@@ -909,10 +909,9 @@ public enum TextfieldType: String {
                             if self.user != nil {
                                 ManageFiles().storeListOfFiles(listOfFileDtos!, forFileId: 0, andUser: self.user!)
                             
-                                app.switchActiveUser(to: self.user, isNewAccount: true, withCompletionHandler:
-                                    {
-                                    app.generateAppInterface(fromLoginScreen: true)
-                                })
+                                app.switchActiveUser(to: self.user, isNewAccount: true)
+                                app.generateAppInterface(fromLoginScreen: true)
+                                
                             } else {
                                 self.showURLError(NSLocalizedString("error_could_not_add_account", comment: ""))
                             }
@@ -928,7 +927,7 @@ public enum TextfieldType: String {
                         if self.loginMode == .migrate {
                             // migration mode needs to start a fresh list of files, so that it is updated with the new URL
                             app.generateAppInterface(fromLoginScreen: true)
-                            
+                              
                         } else {
                             self.closeLoginView()
                         }

Owncloud iOs Client/Tabs/SettingTab/SettingsViewController.m
@@ -791,8 +791,7 @@
     static NSString *CellIdentifier = @"AccountCell";
     
     AccountCell *accountCell = (AccountCell *) [self.settingsTableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    UserDto *userAccount = [UserDto new];
-    userAccount = [self.listUsers objectAtIndex:row];
+    UserDto *userAccount = [self.listUsers objectAtIndex:row];
     
     if (accountCell == nil) {
         
@@ -1104,24 +1103,20 @@
     //We check the connection here because we need to accept the certificate on the self signed server before go to the files tab
     [[CheckAccessToServer sharedManager] isConnectionToTheServerByUrl:[UtilsUrls getFullRemoteServerPath:selectedUser]];
 
-    
     //Method to change the account
     [[OCLoadingSpinner sharedOCLoadingSpinner] initLoadingForViewController: self];
-    [app switchActiveUserTo:selectedUser isNewAccount:NO withCompletionHandler:^{
-        DLog(@"refreshing list of accounts after user was switched");
+    [app switchActiveUserTo:selectedUser isNewAccount:NO];
+    DLog(@"refreshing list of accounts after user was switched");
         
-        [[OCLoadingSpinner sharedOCLoadingSpinner] endLoading];
+    [[OCLoadingSpinner sharedOCLoadingSpinner] endLoading];
         
-        //If ipad, clean the detail view
-        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-            AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication]delegate];
-            [app presentWithView];
-        }
-                
-        [self refreshTable];
-        
-    } ];
+    //If ipad, clean the detail view
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+        AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+        [app presentWithView];
+    }
     
+    [self refreshTable];
 }
 
 #pragma mark - AddAccountDelegate