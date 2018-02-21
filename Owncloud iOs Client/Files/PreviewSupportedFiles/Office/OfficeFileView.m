2e37f97a71e9a65e1d340cbda7d301479318d889
nasli <nalvarez@solidgear.es>
2018-02-21 18:17:50 +0100

    Fix preview of doc files

--- Owncloud iOs Client/Files/PreviewSupportedFiles/Office/OfficeFileView.m
+++ Owncloud iOs Client/Files/PreviewSupportedFiles/Office/OfficeFileView.m
@@ -152,7 +152,11 @@ CGPoint _lastContentOffset;
                 [self.webView  loadHTMLString:[NSString stringWithFormat:@"<div style='font-size:%@;font-family:%@;'><pre>%@",k_txt_files_font_size_ipad,k_txt_files_font_family,dataFile] baseURL:[NSURL URLWithString:@"about:blank"]];
             }
             
-        } else if ([ext isEqualToString:@"TXT"]) {
+        } else if ([ext isEqualToString:@"PDF"]) {
+            NSURL *targetURL = [NSURL fileURLWithPath:filePath];
+            NSData *pdfData = [[NSData alloc] initWithContentsOfURL:targetURL];
+            [self.webView loadData:pdfData MIMEType:@"application/pdf" textEncodingName:@"utf-8" baseURL:url];
+        } else {
             
             NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
             NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:nil];
@@ -162,19 +166,11 @@ CGPoint _lastContentOffset;
             
             NSURLSessionDataTask *task = [session dataTaskWithRequest:headRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
                 dispatch_async(dispatch_get_main_queue(), ^{
-                    [_webView loadData:[NSData dataWithContentsOfURL: url] MIMEType:response.MIMEType textEncodingName:@"utf-8" baseURL:url];
+                    [self.webView loadData:[NSData dataWithContentsOfURL: url] MIMEType:response.MIMEType textEncodingName:@"utf-8" baseURL: [NSURL URLWithString:@"about:blank"]];
                 });
             }];
             
             [task resume];
-            
-        } else if ([ext isEqualToString:@"PDF"]) {
-            NSURL *targetURL = [NSURL fileURLWithPath:filePath];
-            NSData *pdfData = [[NSData alloc] initWithContentsOfURL:targetURL];
-            [self.webView loadData:pdfData MIMEType:@"application/pdf" textEncodingName:@"utf-8" baseURL:url];
-        } else {
-            NSString *dataFile = [[NSString alloc] initWithData:[NSData dataWithContentsOfURL:url] encoding:NSUTF8StringEncoding];
-           [self.webView loadHTMLString:dataFile baseURL: [NSURL URLWithString:@"about:blank"]];
         }
         
         [_webView setHidden:NO];
